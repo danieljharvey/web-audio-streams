160f72a688b56dbb2f9a653ae09d1828a9a6e640
Daniel Harvey <danieljamesharvey@gmail.com>
2017-11-06 21:41:09 +0000

    noise generator

--- js/src/audioCore.js
+++ js/src/audioCore.js
@@ -1,19 +1,62 @@
 
+const createAudioContext = () => new (window.AudioContext || window.webkitAudioContext)();
+
+const createLimiter = audioCtx => {
+  // Create a compressor node
+  const compressor = audioCtx.createDynamicsCompressor();
+  compressor.threshold.value = -1;
+  compressor.knee.value = 40;
+  compressor.ratio.value = 12;
+  compressor.attack.value = 0;
+  compressor.release.value = 0.25;
+  compressor.connect(audioCtx.destination)
+  return compressor
+}
+
+const createOscillator = (audioCtx) => (output) => {
+  const oscillator = audioCtx.createOscillator()
+  oscillator.connect(output)
+  oscillator.type = 'sine';
+  oscillator.frequency.value = 440; // value in hertz
+  return oscillator
+}
+
+const createNoiseOsc = (audioCtx) => (output) => {
+  const node = audioCtx.createBufferSource()
+  const buffer = audioCtx.createBuffer(1, 4096, audioCtx.sampleRate)
+  const data = buffer.getChannelData(0);
+
+  for (var i = 0; i < 4096; i++) {
+    data[i] = Math.random();
+  }
+
+  node.buffer = buffer;
+  node.loop = true;
+  node.connect(output);
+  return node
+}
+
 const startAudio = () => {
   
   console.log('startAudio');
 
   // create web audio api context
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const audioCtx = createAudioContext()
+
+  // create limiter
+  const limiter = createLimiter(audioCtx)
 
   // create Oscillator node
-  var oscillator = audioCtx.createOscillator();
+  const oscillator = createOscillator(audioCtx)(limiter)
 
-  oscillator.type = 'square';
-  oscillator.frequency.value = 440; // value in hertz
-  oscillator.connect(audioCtx.destination);
+  // create Noise node
+  const noiseOsc = createNoiseOsc(audioCtx)(limiter)
+
   oscillator.start();
   oscillator.stop(audioCtx.currentTime + 2);
+
+  noiseOsc.start()
+  noiseOsc.stop(audioCtx.currentTime + 1)
 }
 
 module.exports = {
